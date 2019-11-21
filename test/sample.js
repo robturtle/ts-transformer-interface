b6ebe78bb84954a584efa1fee78f25cb8a83c5bb
Yang Liu <jeremyrobturtle@gmail.com>
2019-11-21 15:45:17 -0800

    update sample.js output

--- test/sample.js
+++ test/sample.js
@@ -7,10 +7,14 @@ var userSchema = {
     { name: 'name', optional: false, type: 'string' },
     { name: 'title', optional: true, type: 'string' },
     { name: 'houses', optional: false, type: { arrayElementType: 'string' } },
-    { name: 'location', optional: false, type: 'Location' },
-    { name: 'spouse', optional: true, type: 'User' },
-    { name: 'children', optional: false, type: { arrayElementType: 'User' } },
-    { name: 'previousLocations', optional: true, type: { arrayElementType: 'Location' } },
+    { name: 'location', optional: false, type: { referenceName: 'Location' } },
+    { name: 'spouse', optional: true, type: { referenceName: 'User' } },
+    { name: 'children', optional: false, type: { arrayElementType: { referenceName: 'User' } } },
+    {
+      name: 'previousLocations',
+      optional: true,
+      type: { arrayElementType: { referenceName: 'Location' } },
+    },
     { name: 'referrer', optional: false, type: null },
   ],
 };
