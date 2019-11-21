379887714029ef1700e9e9dcc6628804a489c4a5
Yang Liu <jeremyrobturtle@gmail.com>
2019-11-20 21:16:33 -0800

    add sample file & script

---
+++ test/compile/index.ts
@@ -0,0 +1,4 @@
+import { compile } from './compile';
+import * as path from 'path';
+
+compile([path.join(__dirname, '../sample.ts')]);
---
+++ test/sample.ts
@@ -0,0 +1,18 @@
+import { schema } from '../index';
+
+interface Location {
+  lat: number;
+  lng: number;
+}
+
+interface User {
+  name: string; // required primitive
+  title?: string; // optional primitive
+  houses: string[]; // required array
+  location: Location; // type reference
+  spouse?: User; // optional type reference
+  children: User[]; // type reference array
+  previousLocations?: Location[]; // optional type reference array
+}
+
+schema<User>();
