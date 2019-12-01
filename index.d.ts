0320e28f704c99d52512362856ca735ecd9cdaf8
Yang Liu <jeremyrobturtle@gmail.com>
2019-11-30 17:39:27 -0800

    add version info

--- index.d.ts
+++ index.d.ts
@@ -1,34 +1,42 @@
 export namespace runtime {
+  /** @since 1.2.0 */
   export type Type = null | string | ArrayType | ReferenceType | ParameterizedType | GenericType;
 
+  /** @since 1.0.0 */
   export interface Property {
     name: string;
     optional: boolean;
     type: Type;
   }
 
+  /** @since 1.0.0 */
   export interface ArrayType {
     arrayElementType: Type;
   }
 
+  /** @since 1.0.0 */
   export interface ReferenceType {
     referenceName: string;
   }
 
+  /** @since 1.2.0 */
   export interface ParameterizedType {
     selfType: string;
     typeArgumentType: Type;
   }
 
+  /** @since 1.2.0 */
   export interface GenericType {
     genericParameterName: string;
     genericParameterType: Type;
   }
 
+  /** @since 1.0.0 */
   export interface Schema {
     name: string;
     props: Property[];
   }
 }
 
+/** @since 1.0.0 */
 export function schema<T extends object>(): runtime.Schema;
